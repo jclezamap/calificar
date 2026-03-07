addb359227ce91f94199801411bf62de72173dbd
Juan Carlos <jclezamap@gmail.com>
2026-03-07 11:36:25 -0500

    Publicada version 0.1.13

--- src/calificar/__init__.py
+++ src/calificar/__init__.py
@@ -1,6 +1,6 @@
 # src/calificar/__init__.py
 
-__version__ = "0.1.12
+__version__ = "0.1.13"
 
 #Se importa los módulos que están en core
 from .core import taller, evafunciones
@@ -13,6 +13,6 @@ __all__ = ["taller", "evafunciones"]
 
 import numpy as np
 
-print(f"Iniciando Calificar v0.1.12 (Usando Numpy {np.__version__})")
+print(f"Iniciando Calificar v0.1.13 (Usando Numpy {np.__version__})")
 
 
