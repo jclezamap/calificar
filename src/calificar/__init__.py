7d7afc2448315875f7c1d7b45d7c314f15239cd2
Juan Carlos <jclezamap@gmail.com>
2026-09-19 11:53:47 -0500

    Publicada version 0.1.21

--- src/calificar/__init__.py
+++ src/calificar/__init__.py
@@ -1,18 +1,17 @@
 # src/calificar/__init__.py
-
-__version__ = "0.1.20"
-
+ 
+__version__ = "0.1.21"
+ 
 #Se importa los módulos que están en core
-from .core import taller, evafunciones
-
-
+from .core import taller, evafunciones, evacolab
+ 
+ 
 # Lo que importa cuando colocan 'from calificar import *'
-__all__ = ["taller", "evafunciones"]
-
-
-
+__all__ = ["taller", "evafunciones", "evacolab"]
+ 
+ 
+ 
 import numpy as np
-
+ 
 print(f"Iniciando Calificar v{__version__} (Usando Numpy {np.__version__})")
-
-
+ 
